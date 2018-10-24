ca931bfb92572e8651a19d85c41f0d7249325874
qiaobahui <qiaobahuiyouxiang@163.com>
2018-10-25 01:14:15 +0800

    基本运算符

--- 2.BasicOperatorsStudy/BasicOperatorsStudy/ViewController.swift
+++ 2.BasicOperatorsStudy/BasicOperatorsStudy/ViewController.swift
@@ -14,6 +14,30 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		// ****************** 比较字符串是否相等 ******************
+		let name1 = "qiaobahui"
+		let name2 = "qiaobahui"
+
+		if name1 == name2 {
+			print("name1 和 name2相等") // 输出: name1 和 name2相等
+		}
+
+
+		// ****************** 三目运算 ******************
+		let contentHeight = 40
+		let hasHeader = true
+		let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+		print(rowHeight) // 输出: 90
+
+
+		// ****************** 空合运算符 ******************
+		let defaultColorName = "red"
+		var userDefinedColorName: String?   //默认值为 nil
+		userDefinedColorName = "Blue"
+
+		let colorNameToUse = userDefinedColorName ?? defaultColorName
+		print(colorNameToUse) // 输出:Blue
 	}
 
 }
