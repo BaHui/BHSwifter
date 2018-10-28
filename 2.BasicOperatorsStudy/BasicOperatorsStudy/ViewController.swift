5b91a3abc7d8e792d5eaa0ab3223420873dd37d3
QiaoBaHui <QiaoBaHui@XiaoBa.local>
2018-10-29 01:23:56 +0800

    更新语法例子

--- 2.BasicOperatorsStudy/BasicOperatorsStudy/ViewController.swift
+++ 2.BasicOperatorsStudy/BasicOperatorsStudy/ViewController.swift
@@ -15,29 +15,49 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		// ****************** 比较字符串是否相等 ******************
-		let name1 = "qiaobahui"
-		let name2 = "qiaobahui"
+		// ****************** 组合赋值运算符******************
+		var num = 100
+		num += 200
+		print("num的值: \(num)") // 输出: 300
 
-		if name1 == name2 {
-			print("name1 和 name2相等") // 输出: name1 和 name2相等
+
+		// ****************** 比较字符串是否相等 **************
+		let nameOne = "qiaobahui"
+		let nameTwo = "qiaobahui"
+
+		if nameOne == nameTwo {
+			print("nameOne 和 nameTwo相等") // 输出: name1 和 name2相等
 		}
 
 
+		let nameThree = "nameThree"
+		let nameFour = "nameFour"
+
+		if nameThree != nameFour {
+			print("nameThree 和 nameFour 不相等") // 输出: nameThree 和 nameFour 不相等
+		}
+
+		// 注意： Swift 也提供恒等（===）和不恒等（!==）这两个比较符来判断两个对象是否引用同一个对象实例。更多细节在类与结构。
+
+
 		// ****************** 三目运算 ******************
-		let contentHeight = 40
+		let contentHeight = 40.0
 		let hasHeader = true
-		let rowHeight = contentHeight + (hasHeader ? 50 : 20)
-		print(rowHeight) // 输出: 90
+		let rowHeight = contentHeight + (hasHeader ? 50.0 : 20.0)
+		print(rowHeight) // 输出: 90.0
 
 
 		// ****************** 空合运算符 ******************
-		let defaultColorName = "red"
+		let defaultColorName = "Red"
 		var userDefinedColorName: String?   //默认值为 nil
-		userDefinedColorName = "Blue"
 
-		let colorNameToUse = userDefinedColorName ?? defaultColorName
-		print(colorNameToUse) // 输出:Blue
+		let colorNameToUse1 = userDefinedColorName ?? defaultColorName
+		print("colorNameToUse1: \(colorNameToUse1)") // 输出: colorNameToUse1: Red
+
+
+		userDefinedColorName = "Blue"
+		let colorNameToUse2 = userDefinedColorName ?? defaultColorName
+		print("colorNameToUse2: \(colorNameToUse2)") // 输出: colorNameToUse2: Blue
 
 		// ****************** 区间 ******************
 		let names = ["name1", "name2", "name3"]
@@ -48,6 +68,38 @@ class ViewController: UIViewController {
 		for name in names[2...] {
 			print(name) //输出: name3
 		}
+
+	// ****************** 逻辑运算符 ******************
+		let allowedEntry = false
+		if !allowedEntry {
+			print("ACCESS DENIED")
+		}
+		// 输出 "ACCESS DENIED"
+
+		let enteredDoorCode = true
+		let passedRetinaScan = false
+		if enteredDoorCode && passedRetinaScan {
+			print("Welcome!")
+		} else {
+			print("ACCESS DENIED")
+		}
+		// 输出 "ACCESS DENIED"
+
+		let hasDoorKey = false
+		let knowsOverridePassword = true
+		if hasDoorKey || knowsOverridePassword {
+			print("Welcome!")
+		} else {
+			print("ACCESS DENIED")
+		}
+		// 输出 "Welcome!"
+
+		if enteredDoorCode && passedRetinaScan || hasDoorKey || knowsOverridePassword {
+			print("Welcome!")
+		} else {
+			print("ACCESS DENIED")
+		}
+		// 输出 "Welcome!"
 
 	}
 
