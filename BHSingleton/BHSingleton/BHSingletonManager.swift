645fabe55dc854682d00a5212ae66ece05bd6906
QiaoBaHui <qiaobahuiyouxiang@163.com>
2018-11-04 09:44:31 +0800

    single

---
+++ BHSingleton/BHSingleton/BHSingletonManager.swift
@@ -0,0 +1,13 @@
+//
+//  BHSingletonManager.swift
+//  BHSingleton
+//
+//  Created by QBH on 2018/11/3.
+//  Copyright © 2018 QBH. All rights reserved.
+//
+
+import UIKit
+
+class BHSingletonManager: NSObject {
+
+}
---
+++ BHSingleton/BHSingleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BHSingleton
+//
+//  Created by QBH on 2018/11/3.
+//  Copyright © 2018 QBH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
